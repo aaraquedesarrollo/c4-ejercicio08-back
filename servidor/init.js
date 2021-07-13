29479081bf847360db3de725032e7035bacc3335
Albert Araque <aaraquedesarrollo@gmail.com>
2021-07-13 12:37:52 +0200

    Index para inciar el servidor y script para inciarlo

--- servidor/init.js
+++ servidor/init.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const express = require("express");
-const debug = require("debug")("api:servidor:servidor");
+const debug = require("debug")("api:servidor:middlewares");
 const chalk = require("chalk");
 const { errorServidor } = require("./errores");
 
